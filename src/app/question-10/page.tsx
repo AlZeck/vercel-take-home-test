c5ec9b1caeffc1cb78e36cf67be187a743b98cfe
Juan <4lzeck@gmail.com>
2023-10-29 20:29:21 +0000

    feat: q 10

--- src/app/question-10/page.tsx
+++ src/app/question-10/page.tsx
@@ -6,7 +6,14 @@ export default function Page() {
       <Link href="/"> {"<"} Go back </Link>
       <h3>{`How could we improve or alter this familiarization exercise?`}</h3>
 
-      <p></p>
+      <p>
+        There is noting to improve, personally I would like some extra time, to
+        run some more through the vercel documentation.
+      </p>
+      <p>
+        I will take some time afterwards to read it more, as it has already
+        given me some ideas for cool projects to put on the platform.
+      </p>
     </>
   );
 }
