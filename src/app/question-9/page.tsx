84bf1bd8e382c1ffe40a8c6e265a43ae4d171bfb
Juan <4lzeck@gmail.com>
2023-10-29 20:26:58 +0000

    feat: question 9

--- src/app/question-9/page.tsx
+++ src/app/question-9/page.tsx
@@ -4,9 +4,23 @@ export default function Page() {
   return (
     <>
       <Link href="/"> {"<"} Go back </Link>
-      <h3>{`What do you think is one of the most common problems which customers ask Vercel for help with? How would you help customers to overcome common problems, short-term and long-term?`}</h3>
+      <h3 className="mb-10">
+        What do you think is one of the most common problems which customers ask
+        Vercel for help with? How would you help customers to overcome common
+        problems, short-term and long-term?
+      </h3>
 
-      <p></p>
+      <p>
+        I think probably one of the most common problems is integrating Vercel
+        with their existing infrastructure, things like how do I migrate my app to Vercel
+        whilst keeping my existing Database in AWS.
+      </p>
+
+      <p>
+        In the short term, writing a small step by step guide specific for the most common
+        scenario. but in the long-term writing a full documentation on adopting Vercel coming from
+        any other platform, to ensure anyone can join.
+      </p>
     </>
   );
 }
